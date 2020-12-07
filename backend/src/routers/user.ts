c9a79e90288e8e8be1501ae5889c4d5f662e50ef
Tommy Long Pham <tpham@smugmug.com>
2020-12-07 11:29:38 -0800

    Add user routers

--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -3,14 +3,35 @@ import User from '../models/User';
 
 const router = express.Router();
 
-router.post('/api/users', async (req, res) => {
+router.post('/api/user', async (req, res) => {
   try {
+    User.init();
     const user = new User(req.body);
     await user.save();
     const token = await user.generateAuthToken();
+    console.log(user, token);
     res.status(201).send({ user, token });
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.post('/api/user/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+
+    if (!user) {
+      return res.status(401).send({
+        error: 'Login failed! Check authentication credentials'
+      });
+    }
+
+    const token = await user.generateAuthToken();
+
+    res.status(200).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error)
   }
 });
 
